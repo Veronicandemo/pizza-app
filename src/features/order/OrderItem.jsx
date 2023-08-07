e814ce95190e054626f633c49a85575751db8822
Veronicandemo <ndemoveronica@gmail.com>
2023-08-07 12:48:14 +0300

    React pizza app

---
+++ src/features/order/OrderItem.jsx
@@ -0,0 +1,24 @@
+import { formatCurrency } from '../../utils/helpers';
+import PropTypes from 'prop-types';
+
+function OrderItem({ item }) {
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li className="py-3 ">
+      <div className="flex items-center justify-between gap-4 text-sm ">
+        <p>
+          <span className="font-bold ">{quantity}&times;</span> {name}
+        </p>
+        <p className="font-bold ">{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+OrderItem.propTypes = {
+  item: PropTypes.node.isRequired,
+  isLoadingIngredients: PropTypes.node.isRequired,
+  ingredients: PropTypes.node.isRequired,
+};
+
+export default OrderItem;
---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,5 @@
+const UserName = () => {
+  return <div className="hidden text-sm font-semibold md:block">Veronica</div>;
+};
+
+export default UserName;
